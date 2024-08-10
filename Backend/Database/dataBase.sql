fa41b32347d6e0f4e96302130076aa1554fb5a67
Pritu15 <pritu.dhar81@gmail.com>
2024-08-10 17:48:40 +0600

    added a column in the user table as user_name

--- Backend/Database/dataBase.sql
+++ Backend/Database/dataBase.sql
@@ -1,8 +1,13 @@
 CREATE DATABASE RunMateDatabase;
-
+--User
 CREATE TABLE User (
     User_id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     image TEXT,
     password VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE
+    IF EXISTS public."User"
+ADD
+    COLUMN user_name "char" NOT NULL;
--- Backend/index.js
+++ Backend/index.js
@@ -14,11 +14,8 @@ app.use(express.json());
 //Create RunMate
 app.post("/createtable", async (req, res) => {
     try {
-        const p = await pool.query(`CREATE TABLE "User" (
-    User_id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    image TEXT,
-    password VARCHAR(255) NOT NULL
+        // const {}
+        const p = await pool.query(`
 )`
         );
 
